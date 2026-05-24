9c8072fd8546ff5c9dda416370af33532517f65f
kashiif373 <kashif.mukhtar950@gmail.com>
2026-05-24 14:18:27 +0530

    Backend deployment ready

--- backend/database.py
+++ backend/database.py
@@ -2,11 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = (
-    "mysql+pymysql://root:Patna%40123@localhost/enterprise_ai"
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL"
 )
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
